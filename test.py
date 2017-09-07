2ed1b84756a5d5aeb7c3c9d5318c1eef79ac2f1d
Drew Duncan <drew.duncan@gmail.com>
2017-09-06 22:11:49 -0400

    Cleaned up node state representation code. Added documentation on representation conventions. Added propensity network test.

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import numpy as np
 from random import choice
 
+# There are two conventions used in this code to represent the state of
+#   a node.
+#   * Asymmetric:  off is represented by 0, on is represented by 1.
+#   * Symmetric:   off is represented by -1, on is represented by 1.
+# The convention used internally may vary from class to class.  The
+#   get_state method will automatically convert to the asymmetric
+#   representation.  The set_state method assumes the input is in
+#   asymmetric representation.
+
 class BoolNetwork:
     def __init__(self, num_nodes, rules):
         self.num_nodes = num_nodes
@@ -9,11 +18,11 @@ class BoolNetwork:
     def next(self):
         self.state = [rule(self.state) for rule in self.rules]
     def get_state(self):
-        return map(lambda x:x*2-1, self.state)
+        return np.array(self.state)
     def set_state(self, state):
-        self.state = map(int, (state + 1.)/2.)
+        self.state = np.array(state)
 
-class PropensityNetwork:
+class PropensityNetwork(BoolNetwork):
     def __init__(self, num_nodes, rules, propensities):
         self.num_nodes = num_nodes
         self.rules = rules
@@ -38,23 +47,30 @@ class TanhNetwork:
     def next(self):
         self.last_state = self.state.copy()
         self.tan_state = np.tanh(self.weights.dot(self.state))
-        self.state[:-1] = (self.tan_state + 1.)/2.
+        self.state[:-1] = sym_to_asym(self.tan_state)
         self.state[:-1] = np.random.rand(self.num_nodes) < self.state[:-1]
-        self.state[:-1] = self.state[:-1]*2. - 1.
+        self.state[:-1] = asym_to_sym(self.state[:-1])
     def train(self, state):
         error = self.state[:-1] - state
         delta_nodes = error * (1-self.tan_state**2)
         self.updates = delta_nodes[np.newaxis].T.dot(self.last_state[np.newaxis])
         self.weights -= self.learning_rate * self.updates
     def set_state(self, state):
-        self.state[:-1] = state
+        self.state[:-1] = asym_to_sym(state)
     def get_state(self):
-        return map(int, self.state[:-1])
+        return sym_to_asym(self.state[:-1])
 
+# Returns a random state vector of length n in asymmetric representation.
 def random_state(n):
-    return np.array([choice([-1.,1.]) for _ in xrange(n)])
+    return np.array([choice([0,1]) for _ in xrange(n)])
 
-# Test of a very simple network.
+def sym_to_asym(a):
+    return (np.array(a) + 1)/2
+
+def asym_to_sym(a):
+    return 2*np.array(a) - 1
+
+# Test of a very simple deterministic boolean network.
 def test1():
     try:
         b = BoolNetwork(4,
@@ -62,7 +78,6 @@ def test1():
                          lambda n:(n[2]),
                          lambda n:(n[3]),
                          lambda n:(n[0])])
-        b.state = [1,0,1,0]
         t = TanhNetwork(4)
         while 1:
             b.set_state(random_state(b.num_nodes))
@@ -77,6 +92,7 @@ def test1():
         print
         print t.weights
 
+# Test of a simple propensity network.
 def test2():
     try:
         b = PropensityNetwork(4,
@@ -84,9 +100,17 @@ def test2():
                          lambda n:(n[2]),
                          lambda n:(n[3]),
                          lambda n:(n[0])],
-                        [(.1,.2), (.3,.4), (.5,.6), (.7,.8)])
-        b.state = [1,0,1,0]
-        b.next()
+                        [(.9,.9)]*4)
+        t = TanhNetwork(b.num_nodes)
+        while 1:
+            b.set_state(random_state(b.num_nodes))
+            t.set_state(b.get_state())
+            b.next()
+            t.next()
+            print b.get_state()
+            print t.get_state()
+            print
+            t.train(b.get_state())
     except KeyboardInterrupt, e:
         print
         print t.weights
